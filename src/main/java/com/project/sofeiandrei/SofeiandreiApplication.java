0dc23caa221a3525bdc5c9921f5a0ee7df4801b5
Sofei Andrei <andrei.sofei@s.unibuc.ro>
2024-01-13 12:58:07 +0200

    Added initial project

---
+++ src/main/java/com/project/sofeiandrei/SofeiandreiApplication.java
@@ -0,0 +1,13 @@
+package com.project.sofeiandrei;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SofeiandreiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SofeiandreiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/sofeiandrei/SofeiandreiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.sofeiandrei;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SofeiandreiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
